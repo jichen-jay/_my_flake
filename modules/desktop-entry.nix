8311a450eccaecfbde0130de85ed4e627a2bc378
jaykchen@icloud.com <jaykchen@icloud.com>
2024-12-08 20:55:25 -0500

    bash -c to execute toggle due to ownership of the mode.sh script

--- modules/desktop-entry.nix
+++ modules/desktop-entry.nix
@@ -4,7 +4,7 @@
       name = "theme-toggle";
       desktopName = "Theme Toggle";
       comment = "Switch between light and dark themes";
-      exec = "/home/jaykchen/.local/bin/xfce4-night-mode.sh toggle";
+      exec = "bash -c /home/jaykchen/.local/bin/xfce4-night-mode.sh toggle";
       icon = "org.xfce.settings.display"; # Use icon name without full path
       terminal = false;
       categories = [ "Settings" "DesktopSettings" "X-XFCE" "Utility" ];
