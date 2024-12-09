712ddd3505a3cf09d0f3e63a4e1d9bae7cfe0593
jaykchen@icloud.com <jaykchen@icloud.com>
2024-12-08 20:42:39 -0500

    use absolute path  exec = "/home/jaykchen/.local/bin/xfce4-night-mode.sh toggle";

--- modules/desktop-entry.nix
+++ modules/desktop-entry.nix
@@ -4,7 +4,7 @@
       name = "theme-toggle";
       desktopName = "Theme Toggle";
       comment = "Switch between light and dark themes";
-      exec = "/run/current-system/sw/share/xfce4/panel/plugins/xfce4-night-mode.sh toggle";
+      exec = "/home/jaykchen/.local/bin/xfce4-night-mode.sh toggle";
       icon = "org.xfce.settings.display"; # Use icon name without full path
       terminal = false;
       categories = [ "Settings" "DesktopSettings" "X-XFCE" "Utility" ];
