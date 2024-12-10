26909bf47cda24983918d8225fa85711af043f3e
jaykchen@icloud.com <jaykchen@icloud.com>
2024-12-09 21:24:22 -0500

    The buildFHSUserEnv function does create an isolated environment that shares some characteristics with containers, but it's not a full-fledged container in the same way that Docker or Podman containers are.

--- modules/fhs-python.nix
+++ modules/fhs-python.nix
@@ -1,22 +1,32 @@
 # /home/jaykchen/_my_flake/modules/fhs-python.nix
 { pkgs, ... }:
 
+let
+  myFhs = pkgs.buildFHSUserEnv {
+    name = "fhs-python-env";
+    targetPkgs = pkgs: (with pkgs; [
+      # List of packages to be available in the FHS environment
+      python311
+      # Add other packages you need here
+    ]);
+    runScript = "bash"; # or any other shell you want
+  };
+in
 {
   # Libraries needed for FHS Python and its packages
   programs.nix-ld.libraries = with pkgs; [
     stdenv.cc.cc.lib
     zlib
     openssl
-    bzip2 # Often needed for Python packages
-    libffi # Foreign Function Interface, used by some Python libs
-    ncurses # For terminal-based applications
-    readline # For interactive input in Python
-    xz # Another compression library sometimes needed
+    bzip2
+    libffi
+    ncurses
+    readline
+    xz
   ];
 
-  environment.systemPackages = with pkgs; [
-    fhs
+  environment.systemPackages = [
     uv
-    python311
+    myFhs
   ];
 }
