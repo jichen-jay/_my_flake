e7adafe71e04b6b5ff38e54b367dab2880902804
jaykchen@icloud.com <jaykchen@icloud.com>
2024-12-21 21:30:20 -0500

    use tempfs for runroot beef up podman config

--- modules/virt.nix
+++ modules/virt.nix
@@ -1,17 +1,42 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.containers.enable = true;
+  virtualisation.containers = {
+    enable = true;
+    containersConf.settings = {
+      engine = {
+        helper_binaries_dir = [ "${pkgs.netavark}/bin" ];
+        runtime_path = [ "${pkgs.runc}/bin/runc" ];
+      };
+    };
+    policy = {
+      default = [ { type = "insecureAcceptAnything"; } ];
+    };
+    registries = {
+      search = [ "docker.io" ];
+    };
+  };
+
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
-    defaultNetwork.settings.dns_enabled = true;
+    defaultNetwork.settings = {
+      dns_enabled = true;
+    };
+    extraPackages = [
+      pkgs.netavark
+      pkgs.fuse-overlayfs
+      pkgs.aardvark-dns
+      pkgs.runc
+    ];
   };
 
   environment.systemPackages = with pkgs; [
     dive
     podman-compose
     podman-tui
+    buildah
+    skopeo
   ];
 
   # environment.persistence."/persist" = {
@@ -20,16 +45,49 @@
   #   ];
   # };
 
-  # Enable Docker socket compatibility
-  virtualisation.podman.dockerSocket.enable = true;
-
   users.users.jaykchen = {
     extraGroups = [ "podman" ];
-    subUidRanges = [{ startUid = 100000; count = 65536; }];
-    subGidRanges = [{ startGid = 100000; count = 65536; }];
+    subUidRanges = [
+      {
+        startUid = 100000;
+        count = 65536;
+      }
+    ];
+    subGidRanges = [
+      {
+        startGid = 100000;
+        count = 65536;
+      }
+    ];
   };
-  # Create Podman network on system activation
-  system.activationScripts.podman-network = ''
-    ${pkgs.podman}/bin/podman network exists podman || ${pkgs.podman}/bin/podman network create podman
-  '';
+
+  fileSystems."/run/jaykchen/1000/containers" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = [
+      "rw"
+      "size=1G"
+      "mode=1777"
+    ];
+  };
+
+  system.activationScripts = {
+    podman-network = ''
+      ${pkgs.podman}/bin/podman network exists podman || ${pkgs.podman}/bin/podman network create podman
+    '';
+    podman-config = ''
+      mkdir -p /etc/containers
+      mkdir -p /var/lib/containers
+      mkdir -p /run/jaykchen/1000/containers
+    '';
+    podman-storage-conf = ''
+            mkdir -p /home/jaykchen/.config/containers
+            cat > /home/jaykchen/.config/containers/storage.conf << EOF
+      [storage]
+      driver = "overlay"
+      graphroot = "/home/jaykchen/.local/share/containers/storage"
+      runroot = "/run/jaykchen/1000/containers"
+      EOF
+    '';
+  };
 }
