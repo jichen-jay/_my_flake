ac7b33a6cabf1a93f756aaf77b558d1e878f86eb
jaykchen@icloud.com <jaykchen@icloud.com>
2024-12-28 09:42:32 -0500

    break out power10k setting

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -26,86 +26,10 @@
     };
 
     initExtra = ''
-      # Enable Powerlevel10k instant prompt
-      if [[ -r "${config.xdg.cacheHome}/p10k-instant-prompt-${config.home.username}.zsh" ]]; then
-        source "${config.xdg.cacheHome}/p10k-instant-prompt-${config.home.username}.zsh"
-      fi
-
-      source ${pkgs.fzf}/share/fzf/key-bindings.zsh
-      source ${pkgs.fzf}/share/fzf/completion.zsh
-      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-      # Core Settings
-      typeset -g POWERLEVEL9K_MODE="unicode-in-256colors"
-      typeset -g POWERLEVEL9K_PROMPT_ADD_NEWLINE=false
-      typeset -g POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-      typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
-      POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
-
-      # Frame Settings
-      # typeset -g POWERLEVEL9K_LEFT_PROMPT_FRAME=true
-      typeset -g POWERLEVEL9K_RIGHT_PROMPT_FRAME=true
-      typeset -g POWERLEVEL9K_FRAME_COLOR=238
-
-      # Path and Prompt Styling
-      typeset -g POWERLEVEL9K_HOST_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_DIR_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_DIR_FOREGROUND=31  # Blue color for path
-      typeset -g POWERLEVEL9K_PROMPT_CHAR_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_VIINS_FOREGROUND=76
-      typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_VIINS_FOREGROUND=196
-
-      # Theme Adaptation
-      typeset -g POWERLEVEL9K_COLOR_SCHEME="light"
-      typeset -g POWERLEVEL9K_COLORS_LEVEL="256"
-      typeset -g POWERLEVEL9K_VISUAL_IDENTIFIER_EXPANSION=' '
-
-      # Remove background colors from segments
-      typeset -g POWERLEVEL9K_VCS_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_STATUS_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_VIRTUALENV_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_NODEENV_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_KUBECONTEXT_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_AWS_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_RAM_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_SWAP_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_IP_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_VPN_IP_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_WIFI_BACKGROUND="none"
-      typeset -g POWERLEVEL9K_BATTERY_BACKGROUND="none"
-
-      # Auto Theme Detection
-      typeset -g POWERLEVEL9K_COLOR_SCHEME="auto"
-      typeset -g POWERLEVEL9K_TERM_SHELL_INTEGRATION=true
-      typeset -g POWERLEVEL9K_VISUAL_IDENTIFIER_EXPANSION=' '
-
-      # Visual Style
-      typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR="·"
-      typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_FOREGROUND=238
-      typeset -g POWERLEVEL9K_MULTILINE_NEWLINE_PROMPT_GAP_CHAR="·"
-      typeset -g POWERLEVEL9K_MULTILINE_LAST_PROMPT_GAP_CHAR="·"
-
-      # Icon Settings
-      typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=""
-      typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON="?"
-      typeset -g POWERLEVEL9K_VCS_UNSTAGED_ICON="!"
-      typeset -g POWERLEVEL9K_VCS_STAGED_ICON="+"
-
-      # Remove Time Display
-      typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
-        status
-        virtualenv
-        nodeenv
-        kubecontext
-        ram
-        swap
-        public_ip
-        vpn_ip
-        wifi
-      )
-
+      source ${./modules/p10k-lean.zsh}
       eval "$(direnv hook zsh)"
     '';
+
     plugins = [
       {
         name = "zsh-autosuggestions";
